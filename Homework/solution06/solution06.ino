27144dda3329e51487414dc27cdcf27793986f94
Duo Chen <DChen@slb.com>
2019-03-28 17:07:16 -0500

    check in code

---
+++ Homework/solution06/solution06.ino
@@ -0,0 +1,23 @@
+/*
+ * Description:
+ *  Use do-while statement to generate 10 random numbers every one second. 
+ *  The random numbers must be less than 100.
+ * 
+ * Author:
+ *  Duo Chen
+ * 
+ * Date:
+ *  3/28/2019
+ */
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int x = 0;
+    do {
+      delay(1000);
+      x = random(100);
+      Serial.println(x);
+    } while (x < 10);
+}
