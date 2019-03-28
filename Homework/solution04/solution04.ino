84b9b578606c99d6d66fb4d4679f25cc8b4de195
Duo Chen <DChen@slb.com>
2019-03-28 17:00:35 -0500

    check in code

---
+++ Homework/solution04/solution04.ino
@@ -0,0 +1,22 @@
+/*
+ * Description:
+ *  Using for statement to print out the following 
+ *  numbers on the serial monitor only once:
+ *  2,3,4,6,9,13,19,28,42,63,94
+ * 
+ * Author:
+ *  Duo Chen
+ * 
+ * Date:
+ *  3/28/2019
+ */
+void setup() {
+  Serial.begin(9600);
+
+  for (int x = 2; x < 100; x = x * 1.5) {
+    Serial.println(x);
+  }
+}
+
+void loop() {
+}
