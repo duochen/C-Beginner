5f2ff430f6d369bf9f5dcbb83557696dcdb6f60f
Duo Chen <DChen@slb.com>
2019-03-28 17:13:01 -0500

    check in code

---
+++ Homework/solution07/solution07.ino
@@ -0,0 +1,22 @@
+/*
+ * Description:
+ *  Use while to print the numbers from 1 to 10.
+ * 
+ * Author:
+ *  Duo Chen
+ * 
+ * Date:
+ *  3/28/2019
+ */
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int var = 1;
+  while (var <= 10) {
+    Serial.println(var);
+    var++;
+  }
+  delay(3000);
+}
